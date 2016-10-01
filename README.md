31c1901d980f1b4520c64aae180cf42273e72039
ErikBoesen <me@erikboesen.com>
2016-10-01 09:50:02 -0400

    Prettier comments

--- README.md
+++ README.md
@@ -4,12 +4,24 @@ A simple chatbot for Vae Victis Team 1418's Discord server.
 
 A work in progress. Feel free to suggest features in the issues.
 
-You'll need to add a token to the end of `bot.py` for this bot to connect to its user counterpart properly.
+## Dependencies
+* [Python 3](https://www.python.org/downloads)
+* `discord.py`
 
-# Contributors
+        pip3 install discord.py
+
+
+## Running
+1. Add the appropriate token to the last line of `bot.py`.
+2. While in `victibot` directory, run:
+
+        python3 bot.py
+
+
+## Contributors
 * [Erik Boesen](https://github.com/ErikBoesen)
 
-# License
-This software is protected under the MIT License. Basically, give credit where such is due and don't claim credit for stuff you didn't do. Oh, and also, if this bot somehow destroys your server or anything else, it's not our fault.
+## License
+This software is protected under the MIT License. Basically, be nice and give credit where such is due. Oh, and also, if you use this bot to do evil things, we're not liable.
 
 More information in `LICENSE`.
--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import discord
 import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 client = discord.Client()
 
@@ -12,29 +15,29 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    #This tells the bot not to do anything if a special command is called
+    # This tells the bot not to do anything if a special command is called
     skip_interaction = False
 
     def interaction(input):
-        #Stuff do do when people ask questions to the bot (i.e. 'where is the robot code' or 'are there any upcoming deadlines', etc...)
-        #I don't mean like siri, but it would be cool to have a bot that is actually helpful
-        if input = "Hello"
-            return "World"
+        # Stuff do do when people ask questions to the bot (i.e. 'where is the robot code' or 'are there any upcoming deadlines', etc...)
+        # I don't mean like siri, but it would be cool to have a bot that is actually helpful
+        if input = 'Hello'
+            return 'World'
 
-    #This stuff is for special commands that require the bot to do more than just respond with an answer
-    #We should standardize this so that all commands begin with a ! or something like that
+    # This stuff is for special commands that require the bot to do more than just respond with an answer
+    # We should standardize this so that all commands begin with a ! or something like that
     if message.content.startswith('!test'):
         client.send_message(message.channel, 'Received test, Victibot is running')
         skip_interaction = True
-    if message.content.startswith('about'):
+    elif message.content.startswith('about'):
         client.send_message(message.channel, 'Running as ' + client.user.name +  '(' + client.user.id + '). Victibot is a work-in-progress chatbot for the team 1418 server. More info on github at https://github.com/ErikBoesen/victibot')
         skip_interaction = True
-    if message.content.startswith('!debug_mode')
+    elif message.content.startswith('!debug_mode')
         skip_interaction = True
-        #Set some variable to do something for some purpose
+        # Set some variable to do something for some purpose
 
-    #Respond to user questions with answers from the interction function
+    # Respond to user questions with answers from the interction function
     if skip_interaction = False:
         client.send_message(message.channel, interaction(message.content))
 
-client.run('')
+client.run('MjMxNTk1NjEwNjgyMjk0Mjcy.CtCx0g.OhIC5GKMiGM6JMbhos5rQcb8FkE')
