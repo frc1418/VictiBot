cc128d3435051e25e8e35277f1574d8ca5839903
ErikBoesen <me@erikboesen.com>
2016-10-01 15:13:34 -0400

    Reorganized message interpretation system

--- bot.py
+++ bot.py
@@ -3,41 +3,39 @@ import asyncio
 
 client = discord.Client()
 
+
+# A dictionary of basic things for the bot to return. More complex (i.e.
+# data-driven) interactions aren't stored here, those go below.
+message_index = {
+    '!ping': 'Pong!',
+    '!hello': 'World!'
+}
+
+
 @client.event
 async def on_ready():
+    """Run when the bot is ready."""
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
+
 @client.event
 async def on_message(message):
-    print(message.author)
-    #if not message.author == 'VictiBot#7518')d:
-    def interaction(input):
-        # This function will be
-        # Stuff do do when people ask questions to the bot (i.e. 'where is the robot code' or 'are there any upcoming deadlines', etc...)
-        # I don't mean like siri, but it would be cool to have a bot that is actually helpful
+    """Catch a user's messages and figure out what to return."""
+    # TODO: Prevent bot from triggering itself.
 
-        message_index = {
-            'Hello': 'World',
-            'VictiBot': 'Victibot is a work-in-progress chatbot for the team 1418 server. More info on GitHub at https://github.com/ErikBoesen/victibot'
-        }
+    # Special returns!
+    if message.content.startswith('!about'):
+        await client.send_message(message.channel, 'Victibot is a chatbot for Team 1418\'s Discord server. Bot is currently running as ' + client.user.name + ' (ID ' + client.user.id + '). View on GitHub: https://github.com/ErikBoesen/victibot')
+    else:
+        # Respond if the message has a basic, static response.
+        # TODO: Apparently 'await' has been replaced in py3 with 'yield from'.
+        # Implement this change.
         try:
-            return message_index[input]
+            await client.send_message(message.channel, message_index[message.content.lower()])
         except:
             pass
-
-    # This stuff is for special commands that require the bot to do more than just respond with an answer
-    # We should standardize this so that all commands begin with a ! or something like that
-    if message.content.startswith('!test'):
-        # TODO: Apparently await has been replaced with yield from.
-        await client.send_message(message.channel, 'Received test, Victibot is running')
-    elif message.content.startswith('about'):
-        await client.send_message(message.channel, 'Running as ' + client.user.name +  '(' + client.user.id + '). Victibot is a work-in-progress chatbot for the team 1418 server. More info on GitHub at https://github.com/ErikBoesen/victibot')
-    else:
-        output = interaction(message.content);
-        if not output == None:
-            await client.send_message(message.channel, output)
 
 client.run('MjMxNTk1NjEwNjgyMjk0Mjcy.CtCx0g.OhIC5GKMiGM6JMbhos5rQcb8FkE')
