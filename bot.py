1a7b95de9e009cfc3d6e0164cb6f35561ade1800
Adrian Hall <muzioclavecin@gmail.com>
2016-10-01 09:42:12 -0400

    Edited bot.py
    Added a more organized command structure and comments
    Have not been able to test, so please don't commit to master until you've made sure it works
    Basically I can't access the actual bot's code and can't test it out yet

--- bot.py
+++ bot.py
@@ -12,7 +12,29 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    #This tells the bot not to do anything if a special command is called
+    skip_interaction = False
+
+    def interaction(input):
+        #Stuff do do when people ask questions to the bot (i.e. 'where is the robot code' or 'are there any upcoming deadlines', etc...)
+        #I don't mean like siri, but it would be cool to have a bot that is actually helpful
+        if input = "Hello"
+            return "World"
+
+    #This stuff is for special commands that require the bot to do more than just respond with an answer
+    #We should standardize this so that all commands begin with a ! or something like that
     if message.content.startswith('!test'):
-        client.send_message(message.channel, 'I recieved a message.')
+        client.send_message(message.channel, 'Received test, Victibot is running')
+        skip_interaction = True
+    if message.content.startswith('about'):
+        client.send_message(message.channel, 'Running as ' + client.user.name +  '(' + client.user.id + '). Victibot is a work-in-progress chatbot for the team 1418 server. More info on github at https://github.com/ErikBoesen/victibot')
+        skip_interaction = True
+    if message.content.startswith('!debug_mode')
+        skip_interaction = True
+        #Set some variable to do something for some purpose
+
+    #Respond to user questions with answers from the interction function
+    if skip_interaction = False:
+        client.send_message(message.channel, interaction(message.content))
 
 client.run('')
