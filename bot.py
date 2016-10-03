bd441f35a0d957f661daa3cb0db87d3434ac847f
ErikBoesen <me@erikboesen.com>
2016-10-03 15:12:28 -0400

    Code cleanup

--- bot.py
+++ bot.py
@@ -15,35 +15,34 @@ message_index = {
 @client.event
 async def on_ready():
     """Run when the bot is ready."""
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print('Logged in as' + client.user.name + ' (ID ' + client.user.id + ').')
     print('------')
 
 
 @client.event
 async def on_message(message):
     """Catch a user's messages and figure out what to return."""
-    # TODO: Prevent bot from triggering itself.
 
-    # Special returns!
-    if message.content.lower().startswith('!about'):
-        await client.send_message(message.channel, 'Victibot is a chatbot for Team 1418\'s Discord server. Bot is currently running as ' + client.user.name + ' (ID ' + client.user.id + '). View on GitHub: https://github.com/ErikBoesen/victibot')
-    elif message.content.lower().startswith('hey victibot'):
-        await client.send_message(message.channel, 'Thanks, but I\'m not Siri (yet).')
-    elif message.content.lower().startswith('ok victibot') or message.content.lower().startswith('okay victibot'):
-        await client.send_message(message.channel, 'Thanks, but I\'m not Google Now (yet).')
-    elif message.content.lower().startswith('xcq') or message.content.lower().startswith('rickroll'):
-        await client.send_message(message.channel, 'Never Gonna Give You Up - Rick Astley: \n https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    elif message.content.lower().startswith('h') or message.content.lower().startswith('H'):
-        await client.send_message(message.channel, 'H has been pressed. H4X1N6 1MM1N3N7.')
-    else:
-        # Respond if the message has a basic, static response.
-        # TODO: Apparently 'await' has been replaced in py3 with 'yield from'.
-        # Implement this change.
-        try:
-            await client.send_message(message.channel, message_index[message.content.lower()])
-        except:
-            pass
+    msg = message.content.lower()
+
+    # Only send back message if user that sent the triggering message isn't a bot
+    if not message.author.bot:
+        # Special returns!
+        if msg.startswith('!about'):
+            await client.send_message(message.channel, 'Victibot is a chatbot for Team 1418\'s Discord server. Bot is currently running as ' + client.user.name + ' (ID ' + client.user.id + '). View on GitHub: https://github.com/ErikBoesen/victibot')
+        elif msg.startswith('hey victibot'):
+            await client.send_message(message.channel, 'Thanks, but I\'m not Siri (yet).')
+        elif msg.startswith('ok victibot') or msg.startswith('okay victibot'):
+            await client.send_message(message.channel, 'Thanks, but I\'m not Google Now (yet).')
+        elif msg.startswith('xcq') or msg.startswith('rickroll'):
+            await client.send_message(message.channel, 'Never Gonna Give You Up - Rick Astley: \n https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        else:
+            # Respond if the message has a basic, static response.
+            # TODO: Apparently 'await' has been replaced in py3 with 'yield from'.
+            # Implement this change.
+            try:
+                await client.send_message(message.channel, message_index[msg])
+            except:
+                pass
 
 client.run('MjMxNTk1NjEwNjgyMjk0Mjcy.CtCx0g.OhIC5GKMiGM6JMbhos5rQcb8FkE')
