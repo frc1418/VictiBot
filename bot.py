a792cde4a1175fd9c06c4ff937a2d72c8ce81461
Adrian-Hall <muzioclavecin@gmail.com>
2016-10-03 17:08:55 -0400

    xkcd function
    pure awesomeness

--- bot.py
+++ bot.py
@@ -1,8 +1,26 @@
 import discord
 import asyncio
-
+import subprocess
 client = discord.Client()
-
+def xkcd():
+    html = subprocess.Popen('curl xkcd.com', shell=True, stdout=subprocess.PIPE).stdout.read()
+    html = html[121:150]
+    html = str(html)
+    comic_title = ""
+    loop = True
+    for i in html:
+        if loop == True:
+            if not i == '<':
+                print (i)
+                comic_title += str(i)
+            else:
+                loop = False
+            if i == "'":
+                print('whyyyyy!!!!')
+    print (comic_title)
+    print ('curl -O imgs.xkcd.com/comics/' + comic_title + '.png')
+    comic_title = comic_title.lower()
+    return 'https://imgs.xkcd.com/comics/' + comic_title[2:] + '.png'
 
 # A dictionary of basic things for the bot to return. More complex (i.e.
 # data-driven) interactions aren't stored here, those go below.
@@ -36,6 +54,8 @@ async def on_message(message):
             await client.send_message(message.channel, 'Thanks, but I\'m not Google Now (yet).')
         elif msg.startswith('xcq') or msg.startswith('rickroll'):
             await client.send_message(message.channel, 'Never Gonna Give You Up - Rick Astley: \n https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        elif msg.startswith('xkcd'):
+            await client.send_message(message.channel,  (xkcd()))
         else:
             # Respond if the message has a basic, static response.
             # TODO: Apparently 'await' has been replaced in py3 with 'yield from'.
@@ -44,5 +64,6 @@ async def on_message(message):
                 await client.send_message(message.channel, message_index[msg])
             except:
                 pass
+
 
 client.run('MjMxNTk1NjEwNjgyMjk0Mjcy.CtCx0g.OhIC5GKMiGM6JMbhos5rQcb8FkE')
