0bca4b5efd4da7d0e445fe816f709b1dd69c881f
Paulo Costa <ashmederheisen@gmail.com>
2016-10-03 10:36:28 -0400

    I'm literally hackerman guys

--- bot.py
+++ bot.py
@@ -35,6 +35,8 @@ async def on_message(message):
         await client.send_message(message.channel, 'Thanks, but I\'m not Google Now (yet).')
     elif message.content.lower().startswith('xcq') or message.content.lower().startswith('rickroll'):
         await client.send_message(message.channel, 'Never Gonna Give You Up - Rick Astley: \n https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    elif message.content.lower().startswith('h ') or message.content.lower().startswith('H '):
+        await client.send_message(message.channel, 'H has been pressed. H4X1N6 1MM1N3N7.')
     else:
         # Respond if the message has a basic, static response.
         # TODO: Apparently 'await' has been replaced in py3 with 'yield from'.
