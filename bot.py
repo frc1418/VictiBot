33c6888e6557ec655a1bcea98f7ec8d145326f7f
ErikBoesen <me@erikboesen.com>
2016-10-13 21:44:08 -0400

    Revamped XKCD system, added some comments

--- bot.py
+++ bot.py
@@ -1,42 +1,13 @@
+# Import various libraries
 import discord
 import asyncio
 import subprocess
-import os
+import requests
+import json
+
+# Initialize bot client
+# TODO: Make bot a class like normal bots.
 client = discord.Client()
-fd = os.open("token.txt",os.O_RDWR)
-token = os.read(fd,100)[2:62]
-token = (str(token)[2:61])
-def xkcd():
-    htmlo = subprocess.Popen('curl xkcd.com', shell=True, stdout=subprocess.PIPE).stdout.read()
-    html = htmlo[121:150]
-    html = str(html)
-    comic_title = ""
-    loop = True
-    for i in html:
-        if loop:
-            if not i == '<':
-                print (i)
-                comic_title += str(i)
-            else:
-                loop = False
-            if i == "'":
-                print('whyyyyy!!!!')
-    print (comic_title)
-    print ('curl -O imgs.xkcd.com/comics/' + comic_title + '.png')
-    comic_title = comic_title.lower()
-    titletext = ''
-    loop = True
-    print (titletext)
-    for a in str(htmlo[2805:]):
-        if loop:
-            if not a == '"':
-                print (a)
-                titletext += str(a)
-            else:
-                print ('something else happened')
-                loop = False
-    print (titletext)
-    return titletext[2:] + '\nhttps://imgs.xkcd.com/comics/' + comic_title[2:] + '.png'
 
 # A dictionary of basic things for the bot to return. More complex (i.e.
 # data-driven) interactions aren't stored here, those go below.
@@ -45,6 +16,9 @@ message_index = {
     '!hello': 'World!',
     'rickroll': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     'xcq': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    '!balloumoji': ':bigdissapointment::moustache::ballouminatti::1982::nope::notapproved::fedora1::happy::flowers::notbad::soundboard:',
+    'it\'s time to stop': 'https://www.youtube.com/watch?v=2k0SmqbBIpQ',
+    'stop': 'https://www.youtube.com/watch?v=2k0SmqbBIpQ'
 }
 
 
@@ -66,19 +40,20 @@ async def on_message(message):
         # Special returns!
         if msg.startswith('!about'):
             await client.send_message(message.channel, 'Victibot is a chatbot for Team 1418\'s Discord server. Bot is currently running as ' + client.user.name + ' (ID ' + client.user.id + '). View on GitHub: https://github.com/ErikBoesen/victibot')
-        elif msg.startswith('hey victibot'):
-            await client.send_message(message.channel, 'Thanks, but I\'m not Siri (yet).')
-        elif msg.startswith('ok victibot') or msg.startswith('okay victibot'):
-            await client.send_message(message.channel, 'Thanks, but I\'m not Google Now (yet).')
-        elif msg.startswith('xcq') or msg.startswith('rickroll'):
-            await client.send_message(message.channel, 'Never Gonna Give You Up - Rick Astley: \n https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        elif msg.startswith('xkcd'):
-            await client.send_message(message.channel,  (xkcd()))
-        elif msg == 'it\'s time to stop' or msg == 'minecraft' or msg == 'league of legends' or msg == 'stop':
-            await client.send_message(message.channel, 'https://www.youtube.com/watch?v=2k0SmqbBIpQ')
+        elif msg.startswith('!xkcd'):
+            # Store the number/other content after the '!xkcd '.
+            comic = msg[6:]
+
+            # If the user included a specific comic number in their message, get the JSON data for that comic. Otherwise, get the JSON data for the most recent comic.
+            r = requests.get('http://xkcd.com/' + comic + '/info.0.json' if comic else 'http://xkcd.com/info.0.json')
+
+            # Send the URL of the image from the JSON fetched above.
+            await client.send_message(message.channel, r.json()['img'])
         elif msg == '!update':
+            # Confirm that the bot is updating
             await client.send_message(message.channel, 'Updating...')
-            print str(subprocess.Popen('git checkout HEAD bot.py', shell=True, stdout=subprocess.PIPE).stdout.read())
+            # Start a git pull to update bot
+            print(str(subprocess.Popen('git pull', shell=True, stdout=subprocess.PIPE).stdout.read()))
         else:
             # Respond if the message has a basic, static response.
             # TODO: Apparently 'await' has been replaced in py3 with 'yield from'.
@@ -89,4 +64,23 @@ async def on_message(message):
                 pass
 
 
+@client.event
+async def on_member_join(member):
+    await client.send_message(member.server, member + ' has joined the server!')
+
+
+@client.event
+async def on_member_remove(member):
+    await client.send_message(member.server, member + ' has left the server!')
+
+
+# Get token from token.txt.
+with open('token.txt', 'r') as token_file:
+    token = token_file.read()
+
+print('Starting with token ' + token + '...')
+
+# Start bot!
 client.run(token)
+
+# That's all, folks.
