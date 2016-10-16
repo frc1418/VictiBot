b7dd05e222aa1693518b44ef312deca3e72886c3
Michael Cao <mike.x.cao@gmail.com>
2016-10-15 20:02:29 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,12 +66,12 @@ async def on_message(message):
 
 @client.event
 async def on_member_join(member):
-    await client.send_message(member.server.default_channel, member.display_name + ' has joined the server!')
+    await client.send_message(member.server.default_channel, '**Welcome ' + member.display_name + ' to the ' + member.server.name + 'server!**')
 
 
 @client.event
 async def on_member_remove(member):
-    await client.send_message(member.server.default_channel, member.display_name + ' has left the server!')
+    await client.send_message(member.server.default_channel, member.display_name + ' has left the server :frowning: RIP ' + member.display_name)
 
 
 # Get token from token.txt.
