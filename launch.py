8b41f2a6c97620ecb0cc208f3639e51a41f10dcd
Adrian-Hall <muzioclavecin@gmail.com>
2016-10-18 15:48:42 -0400

    sdfd

--- launch.py
+++ launch.py
@@ -1,7 +1,7 @@
 import subprocess
 execute = True
 while execute == True:
-    if (subprocess.Popen('cat run', shell=True, stdout=subprocess.PIPE).stdout.read()) == "1":
+    if (subprocess.Popen('cat run', shell=True, stdout=subprocess.PIPE).stdout.read())[2:3] == "1":
         subprocess.Popen('killall bot', shell=True, stdout=subprocess.PIPE).stdout.read()
         subprocess.Popen('python3 bot.py&', shell=True, stdout=subprocess.PIPE).stdout.read()
-    print (subprocess.Popen('cat run', shell=True, stdout=subprocess.PIPE).stdout.read())
+    print (subprocess.Popen('cat run', shell=True, stdout=subprocess.PIPE).stdout.read())[2:3]
