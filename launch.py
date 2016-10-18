8eca92812d6f93c816ac5685ee5877a99997dd7c
Adrian-Hall <muzioclavecin@gmail.com>
2016-10-18 15:49:25 -0400

    sdks

--- launch.py
+++ launch.py
@@ -4,4 +4,4 @@ while execute == True:
     if (subprocess.Popen('cat run', shell=True, stdout=subprocess.PIPE).stdout.read())[2:3] == "1":
         subprocess.Popen('killall bot', shell=True, stdout=subprocess.PIPE).stdout.read()
         subprocess.Popen('python3 bot.py&', shell=True, stdout=subprocess.PIPE).stdout.read()
-    print (subprocess.Popen('cat run', shell=True, stdout=subprocess.PIPE).stdout.read())[2:3]
+    print (subprocess.Popen('cat run', shell=True, stdout=subprocess.PIPE).stdout.read()[2:3])
