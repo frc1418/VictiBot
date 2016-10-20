2eca737ebb056cd75a981d1a23fb6bd53d41a080
Tim Winters <twinters007@gmail.com>
2016-10-20 00:03:06 -0400

    Caps checking works and removed alert

--- bot.py
+++ bot.py
@@ -69,9 +69,10 @@ async def on_message(message):
             subprocess.Popen('echo ' + message.channel + ' | cat > LastId', shell=True, stdout=subprocess.PIPE).stdout.read()
             # Restart
             os.system('python3 launch.py')
-        elif msg.isUpper() and msg.len() > 5:
+        elif message.content.isupper() and len(message.content) > 5:
             #if someone sends a message in all caps, respond with a friendly reminder
-            await client.send_message(message.channel "did that _really_ need to be in all caps? ")
+            print("caps")
+            await client.send_message(message.channel, "did that _really_ need to be in all caps?")
         else:
             # Respond if the message has a basic, static response.
             # TODO: Apparently 'await' has been replaced in py3 with 'yield from'.
