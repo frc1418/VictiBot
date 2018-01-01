b9e44df0cf0b5838299cec3578068d009d9fc820
ErikBoesen <me@erikboesen.com>
2018-01-01 14:41:12 -0500

    Add warning when talking in #github for too long.

--- bot.py
+++ bot.py
@@ -33,35 +33,47 @@ class VictiBot(discord.Client):
             content = None
 
         # Only send back message if user that sent the triggering message isn't a bot
-        if not message.author.bot and cmd is not None:
-            print('RECIEVED: %s, %s from %s' % (cmd, content, message.author))
-            if cmd == 'about':
-                await self.send_message(message.channel, 'VictiBot is a chatbot for Team 1418\'s Discord server. Bot is currently running as ' + self.user.name + ' (ID ' + self.user.id + '). View on GitHub: https://github.com/frc1418/victibot')
-            elif cmd == 'xkcd':
-                # If the user included a specific comic number in their message, get the JSON data for that comic. Otherwise, get the JSON data for the most recent comic.
-                comic = requests.get('http://xkcd.com/' + content[0] + '/info.0.json' if content else 'http://xkcd.com/info.0.json').json()
+        if not message.author.bot:
+            if cmd is not None:
+                print('RECIEVED: %s, %s from %s' % (cmd, content, message.author))
+                if cmd == 'about':
+                    await self.send_message(message.channel, 'VictiBot is a chatbot for Team 1418\'s Discord server. Bot is currently running as ' + self.user.name + ' (ID ' + self.user.id + '). View on GitHub: https://github.com/frc1418/victibot')
+                elif cmd == 'xkcd':
+                    # If the user included a specific comic number in their message, get the JSON data for that comic. Otherwise, get the JSON data for the most recent comic.
+                    comic = requests.get('http://xkcd.com/' + content[0] + '/info.0.json' if content else 'http://xkcd.com/info.0.json').json()
 
-                # Send the URL and hover text of the comic.
-                await self.send_message(message.channel, comic['img'])
-                await self.send_message(message.channel, comic['alt'])
-            elif cmd == 'nasa':
-                # Get JSON data from NASA APOD API
-                photo = requests.get('https://api.nasa.gov/planetary/apod?api_key=%s' % (self.config['apod_key'] if self.config['apod_key'] else 'DEMO_KEY')).json()
+                    # Send the URL and hover text of the comic.
+                    await self.send_message(message.channel, comic['img'])
+                    await self.send_message(message.channel, comic['alt'])
+                elif cmd == 'nasa':
+                    # Get JSON data from NASA APOD API
+                    photo = requests.get('https://api.nasa.gov/planetary/apod?api_key=%s' % (self.config['apod_key'] if self.config['apod_key'] else 'DEMO_KEY')).json()
 
-                # Send URL for image along with image's title
-                await self.send_message(message.channel, photo['url'])
-                await self.send_message(message.channel, photo['title'])
-            elif cmd == 'help':
-                # PM the user a help message.
-                await self.send_message(message.author, open('help.md').read())
-            else:
-                # Respond if the message has a basic, static response.
-                try:
-                    with open('commands.json') as f:
-                        commands = json.load(f)
-                    await self.send_message(message.channel, commands[cmd])
-                except KeyError:
-                    pass
+                    # Send URL for image along with image's title
+                    await self.send_message(message.channel, photo['url'])
+                    await self.send_message(message.channel, photo['title'])
+                elif cmd == 'help':
+                    # PM the user a help message.
+                    await self.send_message(message.author, open('help.md').read())
+                else:
+                    # Respond if the message has a basic, static response.
+                    try:
+                        with open('commands.json') as f:
+                            commands = json.load(f)
+                        await self.send_message(message.channel, commands[cmd])
+                    except KeyError:
+                        pass
+
+            if message.channel.name == 'github':
+                print('Message recieved in #github from ' + message.author.name)
+                contains_hook = False
+                async for msg in self.logs_from(message.channel, limit=6):
+                    if msg.author.name == 'GitHub':
+                        contains_hook = True
+                        break
+                if not contains_hook:
+                    print('No hook found in past 6 messages, sending warning.')
+                    await self.send_message(message.channel, 'Psst... you may want to move to %s.' % self.get_channel('228121923245178880').mention)
 
     async def on_member_join(self, member):
         """
